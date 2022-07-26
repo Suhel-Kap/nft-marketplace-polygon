e32fc0361fe6fb5953df53642294cdab16f11bda
Suhel-Kap <suhelkapadia2@gmail.com>
2022-07-27 00:38:01 +0530

    basic done

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     }, [nfts])
 
     const loadNfts = async () => {
-        const provider = new ethers.providers.JsonRpcProvider()
+        const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com/")
         const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider)
         const marketContract = new ethers.Contract(nftMarketddress, NFTMarketplace.abi, provider)
         const data = await marketContract.fetchMarketItems()
